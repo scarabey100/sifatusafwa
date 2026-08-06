55459c776be480378fed853a60bade6e5b323c26
Alex <tocker999@gmail.com>
2026-08-06 18:44:54 +0300

    SFS-39 {Catalog Feature} Task: Adding the alignProductFlags method

--- themes/sifatusafwa/assets/js/product-flags-custom.js
+++ themes/sifatusafwa/assets/js/product-flags-custom.js
@@ -17,5 +17,32 @@
         flags.style.removeProperty('--eg-image-height');
     }
 
+    function alignProductFlags() {
+        var pageContent = document.querySelector('#product .product__media .page-content');
+        var flags = pageContent && pageContent.querySelector(':scope > .product-flags');
+        var image = pageContent && pageContent.querySelector('.magic-slide.mt-active a.MagicZoom');
+
+        if (!pageContent || !flags || !image) {
+            resetProductFlags(flags);
+            return;
+        }
+
+        var containerRect = pageContent.getBoundingClientRect();
+        var imageRect = image.getBoundingClientRect();
+
+        // A failed/not-yet-loaded Magic Zoom image can report a 2x2px anchor.
+        // Keep the original container-based positioning until a real image exists.
+        if (imageRect.width < 50 || imageRect.height < 50) {
+            resetProductFlags(flags);
+            return;
+        }
+
+        flags.classList.add('eg-image-bound');
+        flags.style.setProperty('--eg-image-left', (imageRect.left - containerRect.left) + 'px');
+        flags.style.setProperty('--eg-image-top', (imageRect.top - containerRect.top) + 'px');
+        flags.style.setProperty('--eg-image-width', imageRect.width + 'px');
+        flags.style.setProperty('--eg-image-height', imageRect.height + 'px');
+    }
+
 
 }());
